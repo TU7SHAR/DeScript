82003b67bce4e77058ade43334df560659646115
TU7SHAR <brahmin00011@gmail.com>
2024-11-17 11:05:44 +0530

    Deploy 2

--- app/Telegram/Success/page.jsx
+++ app/Telegram/Success/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React from "react";
-import { useSearchParams } from "next/navigation.js";
+import React, { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 
-function page() {
+function SuccessPage() {
   let out = useSearchParams().get("out");
   const ctr = (out.match(/successfully left/gi) || []).length;
   out = out.replace(/}$/, "");
@@ -14,7 +14,7 @@ function page() {
     <div className="flex items-center justify-center min-h-screen ">
       <div className=" p-8 bg-gray-900 rounded-lg shadow-lg text-center">
         <h1 className="text-3xl font-bold mb-4">Script Success</h1>
-        <p className="text-gray-500  whitespace-pre-wrap">
+        <p className="text-gray-500 whitespace-pre-wrap">
           {out ? out : "Loading..."}
         </p>
         <p className="text-gray-500 mb-4 whitespace-pre-wrap">
@@ -28,4 +28,10 @@ function page() {
   );
 }
 
-export default page;
+export default function WrappedSuccessPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SuccessPage />
+    </Suspense>
+  );
+}
